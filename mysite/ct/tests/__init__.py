e9c18d39210212f6b5de848a96f2312e7623beb3
Dmitry <derdmitry@gmail.com>
2015-09-18 17:01:53 +0300

    comment selenium tests

--- mysite/ct/tests/__init__.py
+++ mysite/ct/tests/__init__.py
@@ -2,7 +2,7 @@ from integrate import *
 from views import *
 from models import *
 from forms import *
-from selenium_integrate import *
+#from selenium_integrate import *
 from plugins import *
 from templatetags import *
 from ct_util import *
