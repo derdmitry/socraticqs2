5e1fdb1f804e38bef93bde2ef8892932313d758b
Dmitry <derdmitry@gmail.com>
2015-10-27 13:49:15 +0200

    Add deploy on dev
    Add fabric task for deploy on dev server

--- fabfile/deploy/__init__.py
+++ fabfile/deploy/__init__.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 from fabric.contrib import django
-from fabric.api import local, lcd
+from fabric.api import local, run, lcd, cd
 from fabric.tasks import Task
 
 from fab_settings import env
@@ -13,46 +13,69 @@ sys.path.append(os.path.dirname(__file__) + '/../../mysite/')
 django.settings_module('mysite.settings')
 
 STAGING_BRANCH = 'master'
-BASE_PATH = os.path.dirname(__file__) + '/../..'
-LOCAL_SETTINGS_PATH = BASE_PATH + '/../settings'
+BASE_PATH = os.path.dirname(__file__)
 
-
-class Staging(Task):
+class Deploying(Task):
     """
-    Deploy project on staging
+    Deploy project
     """
 
-    def virtualenv(self):
-        with lcd(BASE_PATH + '/../'):
-            local('source {}/bin/activate'.format(env.venv_name))
+    func = local
+    func_cd = lcd
+    __code_branch = STAGING_BRANCH
+    __project_path = os.path.join(BASE_PATH, '/../..')
+    __local_settings_path = os.path.join(__project_path, '/../settings')
+
+    def __virtualenv(self):
+        with self.func_cd(os.path.join(self.__project_path, '/../')):
+            self.func('source {}/bin/activate'.format(env.venv_name))
 
     def update_requirements(self):
-        with lcd(BASE_PATH):
-            local("pip install -r prod_requirements.txt")
+        with self.func_cd(self.__project_path):
+            self.func("pip install -r requirements.txt")
 
-    def get_settings(self):
-        with lcd(LOCAL_SETTINGS_PATH):
-            local('git pull origin master')
-            local('cp developer_conf.py ../socraticqs2/mysite/mysite/developer_conf.py')
+    def __get_settings(self, branch='master'):
+        with self.func_cd(self.__local_settings_path):
+            self.func('git pull origin {0}'.format(branch))
+            self.func('cp local_conf_example.py ../socraticqs2/mysite/mysite/local_conf.py')
 
-    def restart_service(self):
-        local('sudo supervisorctl restart gunicorn')
-        local('sudo supervisorctl restart celery')
-        local('sudo service nginx restart')
+    def __restart_service(self):
+        self.func('sudo supervisorctl restart gunicorn')
+        self.func('sudo supervisorctl restart celery')
+        self.func('sudo service nginx restart')
 
-    def update(self):
-        local('git pull origin %s' % STAGING_BRANCH)
-        self.get_settings()
-        local('find . -name "*.pyc" -print -delete')
-        self.virtualenv()
+    def __update(self):
+        self.func('git pull origin %s' % self.__code_branch)
+        self.__get_settings()
+        self.func('find . -name "*.pyc" -print -delete')
+        self.__virtualenv()
         self.update_requirements()
-        with lcd("mysite"):
-            local('python manage.py collectstatic --noinput')
-            local('python manage.py syncdb --noinput')
-        self.restart_service()
+        with self.func_cd("mysite"):
+            self.func('python manage.py collectstatic --noinput')
+            self.func('python manage.py syncdb --noinput')
+        self.__restart_service()
 
-    def run(self, suffix=None):
-        self.update()
+    def run(self, running='local', branch='master', suffix=None):
+        if running == 'local':
+            self.func = local
+            self.func_cd = lcd
+        elif running == 'server':
+            self.func = run
+            self.func_cd = cd
+
+        self.__code_branch = branch
+        self.__update()
 
 
-staging = Staging()
+class Development(Deploying):
+
+    __project_path = os.path.join(BASE_PATH, '/../../dev')
+    __code_branch = 'dev'
+
+    def __get_settings(self):
+        """On dev staging we don't use prodaction settings"""
+        pass
+
+
+staging = Deploying()
+dev = Development()
